e034b46a30a7039abe5e5166c7b79475afd7c940
jatin mahajan <jatinmahajan711@gmail.com>
2024-09-12 22:48:50 -0400

    first commit

---
+++ duplicate.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasDuplicate(self, nums) -> bool:
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                return True
+            dict[nums[i]] = 1
+        return False
+    
+
+
+instance = Solution()
+
+print(instance.hasDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 1, 10])) # False
+
+            
+
+         
+         
